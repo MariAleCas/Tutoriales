87b55ec24a6e6e0ddfbbdb4020abb5e8a6fb034f
MariAleCas <maalbecas@gmail.com>
2021-09-20 10:06:27 -0500

    <tener en cuenta los metodos que se peude usar en Js

--- react/tutorial-1/src/App.js
+++ react/tutorial-1/src/App.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react'
 import Table from './table'
 class App extends React.Component {
+  state = {
+    characters: [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+    ]
+  }
   render() {
-    return (<div className="container">
-      <Table />
-    </div>
+    const { characters } = this.state
+    return (
+      <div className="container">
+        <Table charactersData={characters} removecharacter={this.removecharacter} />
+      </div>
     )
+  }
+  removecharacter = (index) => {
+    const { characters } = this.state
+    this.setState({
+      characters: characters.filter((characters, i) => {
+        return i !== index
+      }),
+    })
   }
 }
 export default App
--- react/tutorial-1/src/table.js
+++ react/tutorial-1/src/table.js
@@ -2,16 +2,25 @@ import React, { Component } from "react"
 
 class Table extends Component {
   render() {
+    const { charactersData } = this.props
     return (
       <table>
-        <TableBody />
+        <TableBody charactersData={charactersData} />
         <TableHeader />
       </table>
     )
   }
 }
-const TableBody = () => {
-  return <tbody />
+const TableBody = (props) => {
+  const rows = props.charactersData.map((row, index) => {
+    return (
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+      </tr>
+    )
+  })
+  return <tbody>{rows}</tbody>
 }
 const TableHeader = () => {
   return (
