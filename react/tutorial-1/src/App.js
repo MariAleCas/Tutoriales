fd6afee62f619e64fc7bcd60c0736d28b3cc27cb
MariAleCas <maalbecas@gmail.com>
2021-09-24 15:19:49 -0500

    < No recuerdo el cambio -_- >

--- react/tutorial-1/src/App.js
+++ react/tutorial-1/src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
 import Table from './table'
+import React, { Component } from 'react'
 import Form from './Form'
 class App extends React.Component {
   state = {
