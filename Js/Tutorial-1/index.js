fb54bfcc7a28f75b0eb6ca29a9d6fb18da72d7b5
MariAleCas <maalbecas@gmail.com>
2021-09-24 15:18:25 -0500

    < Mejorando codigo *w* >

--- Js/Tutorial-1/index.js
+++ Js/Tutorial-1/index.js
@@ -1,41 +1,57 @@
-window.addEventListener('load', () => {
-  const diaplay = document.querySelector('.calculator-display');
-  const keypadButtons = document.getElementsByClassName('keypad-button');
-  const keypadButtonsArray = Array.from(keypadButtons);
 
-  keypadButtonsArray.forEach((button) => {
+//Se le da el evento 'load' a window, luego se ejecuta la función
+
+window.addEventListener('load', load)
+//se encarga de enlistar los elementos div de la clase keypad-button
+//
+function load() {
+  const display = document.querySelector('.calculator-display')
+  console.log(display)
+  const keypadButtons = document.getElementsByClassName('keypad-button')
+  const keypadButtonsArray = Array.from(keypadButtons)
+  console.log(keypadButtonsArray)
+  //button representa los botones de la calculadora y cada vez que se de click se ejecuta a la función
+  keypadButtonsArray.forEach(button => {
     button.addEventListener('click', () => {
-      calculadora(button, diaplay)
+      calculadora(button, display)// callback
     })
   })
-
-});
+}
 
 function calculadora(button, display) {
+  console.log(display, button)
+  console.log(display.innerHTML, button.innerHTML)
   switch (button.innerHTML) {
     case 'C':
-      borrar(display);
-      break;
+      borrar(display)
+      break
     case '=':
       calcular(display);
       break
     default:
       actualizar(display, button)
-      break;
+      break
   }
 }
 
-function calcular(display) {
+/*function calcular(display) {
+  //eval trabaja con caracteres
   display.innerHTML = eval(display.innerHTML)
-}
+}*/
+
+const calcular = (display) => display.innerHTML = eval(display.innerHTML)
+
+//const actualizar = (display, button) => !display.innerHTML ? '' : display.innerHTML += button.innerHTML
+
+const borrar = (display) => display.innerHTML = 0
 
 function actualizar(display, button) {
   if (display.innerHTML == 0) {
-    display.innerHTML = '';
+    display.innerHTML = ''
   }
   display.innerHTML += button.innerHTML;
+  console.log(display.innerHTML)
 }
-
-function borrar(display) {
+/*function borrar(display) {
   display.innerHTML = 0
-}
+}*/
