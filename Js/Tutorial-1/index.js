a0f516f54f945ef42b18fd9970daf454d879eff5
MariAleCas <maalbecas@gmail.com>
2021-09-30 14:31:49 -0500

    < Terminado *w*>

--- Js/Tutorial-1/index.js
+++ Js/Tutorial-1/index.js
@@ -1,15 +1,11 @@
-
 //Se le da el evento 'load' a window, luego se ejecuta la función
-
 window.addEventListener('load', load)
 //se encarga de enlistar los elementos div de la clase keypad-button
-//
 function load() {
   const display = document.querySelector('.calculator-display')
   console.log(display)
   const keypadButtons = document.getElementsByClassName('keypad-button')
   const keypadButtonsArray = Array.from(keypadButtons)
-  console.log(keypadButtonsArray)
   //button representa los botones de la calculadora y cada vez que se de click se ejecuta a la función
   keypadButtonsArray.forEach(button => {
     button.addEventListener('click', () => {
@@ -34,24 +30,25 @@ function calculadora(button, display) {
   }
 }
 
+const calcular = (display) => display.innerHTML = eval(display.innerHTML)
+
+const borrar = (display) => display.innerHTML = 0
+
+const actualizar = (display, button) => {
+  if (display.innerHTML == 0) {
+    display.innerHTML = ''
+  }
+  display.innerHTML += button.innerHTML
+}
+
+
 /*function calcular(display) {
   //eval trabaja con caracteres
   display.innerHTML = eval(display.innerHTML)
 }*/
 
-const calcular = (display) => display.innerHTML = eval(display.innerHTML)
-
 //const actualizar = (display, button) => !display.innerHTML ? '' : display.innerHTML += button.innerHTML
 
-const borrar = (display) => display.innerHTML = 0
-
-function actualizar(display, button) {
-  if (display.innerHTML == 0) {
-    display.innerHTML = ''
-  }
-  display.innerHTML += button.innerHTML;
-  console.log(display.innerHTML)
-}
 /*function borrar(display) {
   display.innerHTML = 0
 }*/
